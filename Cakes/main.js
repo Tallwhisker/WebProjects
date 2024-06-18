f250b766b689068d20861df11c982c50fc94f072
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-06-18 18:10:57 +0200

    Most of CSS done

--- Cakes/main.js
+++ Cakes/main.js
@@ -1,10 +1,4 @@
 
-
-//Item to promote
-const promotion = [
-    "name", "pic" , 25
-];
-
 //All the cupcakes
 const cupcakes = [
     ["name", "pic", 25]
@@ -28,3 +22,21 @@ const cakes = [
     ["name", "pic", 25]
     ["name", "pic", 25]
 ];
+
+
+//Select 1 random for the promotion
+
+function makeAPromotion() {
+    let cupcakeCount = cupcakes.length();
+    let cakeCount = cakes.length();
+
+    let luckyProduct = Math.floor(Math.random() * (cupcakeCount + cakeCount));
+
+
+    if (luckyProduct > cupcakeCount -1) {
+        //Subtract first arrays length
+        luckyProduct -= cupcakeCount;
+
+        cakes[luckyProduct];
+    }
+}
