2e45f482f98e089ae2b8880828d9fab77847810e
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-06-18 11:51:14 +0200

    Foundational update

---
+++ Cakes/main.js
@@ -0,0 +1,30 @@
+
+
+//Item to promote
+const promotion = [
+    "name", "pic" , 25
+];
+
+//All the cupcakes
+const cupcakes = [
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+];
+
+//All the cakes
+const cakes = [
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+    ["name", "pic", 25]
+];
