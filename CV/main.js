0fbb8012c86e6f1ad8b091d52efa6d7624372904
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-06-25 22:03:38 +0200

    animations

--- CV/main.js
+++ CV/main.js
@@ -1,19 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-
-// window.onload = function getPerson() {
-//     fetch(`./kevin.json`)
-//     .then(response => {
-//         if(!response.ok) {
-//             throw new Error('FetchItemInfo Error');
-//         };
-//         return response.json();
-//     })
-//     .then(data => {
-//         extractObject(data);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
-// };
-// body = document.querySelector("body");
-
+const hiddenElements = document.querySelectorAll('p');
+hiddenElements.forEach((el) => observer.observe(el));
