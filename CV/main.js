0a72e84029d0e6c398dc62f929a4774efab7fe3d
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-06-25 20:54:26 +0200

    NerdPage

---
+++ CV/main.js
@@ -0,0 +1,19 @@
+
+
+// window.onload = function getPerson() {
+//     fetch(`./kevin.json`)
+//     .then(response => {
+//         if(!response.ok) {
+//             throw new Error('FetchItemInfo Error');
+//         };
+//         return response.json();
+//     })
+//     .then(data => {
+//         extractObject(data);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+// };
+// body = document.querySelector("body");
+
